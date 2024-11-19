83e5a415a498ca16a7ec651d323cb44ec0317177
Alejandroox <darmdz23@gmail.com>
2024-11-19 17:07:51 -0400

    Create ecosystem.config.js

---
+++ src/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+      {
+        name: 'mi-servidor-whatsapp',
+        script: 'src/app.ts', // Asegúrate de que este sea el archivo correcto
+        instances: 1,
+        exec_mode: 'fork', // O 'cluster' si quieres usar múltiples instancias
+        watch: false, // Cambia a true si deseas reiniciar en cambios
+        env: {
+          NODE_ENV: 'production', // Puedes agregar otras variables de entorno si es necesario
+        },
+      },
+    ],
+  };
