10dec3a458f8084d35387b54564a208cb69859ca
Alejandroox <darmdz23@gmail.com>
2025-03-11 14:33:57 -0400

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,6 +4,8 @@ module.exports = {
       name: "API",
       script: "./node_modules/.bin/tsm",
       args: "src/app.ts",
+      instances: 'max',
+      exec_mode: 'cluster',
       watch: false, // Opcional: habilita el modo de vigilancia
       env: {
         NODE_ENV: "production"
