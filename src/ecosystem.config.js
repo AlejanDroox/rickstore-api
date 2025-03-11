ae251bdf6588a9928885318c7340869b79d4121c
Alejandroox <darmdz23@gmail.com>
2025-03-11 14:07:39 -0400

    new ecosistem

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -1,13 +1,17 @@
 module.exports = {
   apps: [
     {
-      name: "API",
-      script: "./node_modules/.bin/tsm",
-      args: "src/app.ts",
-      watch: false, // Opcional: habilita el modo de vigilancia
+      name: 'api-whatsapp2',
+      script: 'dist/app.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      watch: true,
       env: {
-        NODE_ENV: "production"
-      }
-    }
-  ]
+        NODE_ENV: 'production',
+      },
+      env_development: {
+        NODE_ENV: 'development',
+      },
+    },
+  ],
 };
